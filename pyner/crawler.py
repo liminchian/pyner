4bfa680feb9978f449f4cd8d33beafd55d6ccc01
liminchian <liminchian@gmail.com>
2022-06-28 23:35:42 +0800

    add parse function and Crawler

--- pyner/crawler.py
+++ pyner/crawler.py
@@ -3,21 +3,44 @@ import requests
 from bs4 import BeautifulStoneSoup
 
 
-def response(headers=None, body=None):
-    def inner(url: str, method: str):
+def parse(func):
+    def inner(obj, *args, **kwargs):
+        return func(obj, *args, **kwargs)
+
+    return inner
+
+
+# Web selector and find element by html string.
+css_selector = parse(BeautifulStoneSoup.select)
+xpath_selector = parse(libxml2.xpathContext.xpathEval)
+tag = parse(BeautifulStoneSoup.find_all)
+
+# Build html document.
+xpath_parser = parse(libxml2.parseDoc)
+soup = parse(BeautifulStoneSoup)
+
+
+class Crawler(object):
+    def __init__(
+        self,
+        url: str,
+        method: str,
+        headers=None,
+        body=None,
+        proxies=None,
+    ) -> None:
+        self._url = url
+        self._method = method
+        self._headers = headers
+        self._body = body
+        self._proxies = proxies
+
+    def response(self) -> str:
         with requests.Session() as rs:
-            return rs.request(method, url, headers=headers, data=body).text
-
-    return inner
-
-
-def selector(func):
-    def inner(html, *args, **kwargs):
-        return func(html, *args, **kwargs)
-
-    return inner
-
-
-css_selector = selector(BeautifulStoneSoup.select)
-tag_selector = selector(BeautifulStoneSoup.find_all)
-xpath_selector = selector(libxml2.xpathContext.xpathEval)
+            return rs.request(
+                method=self._method,
+                url=self._url,
+                headers=self._headers,
+                data=self._body,
+                proxies=self._proxies,
+            ).text
